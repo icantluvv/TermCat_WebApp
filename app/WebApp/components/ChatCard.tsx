9f33f6994283e6cb672857c04a04d61772be8d49
Sergey <spanteleev670@gmail.com>
2025-06-14 17:37:07 +0300

    rename png at chat card

--- app/WebApp/components/ChatCard.tsx
+++ app/WebApp/components/ChatCard.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 import Image from "next/image"
-import AI from "@/public/images/homepage/ai.png"
+import AI from "@/public/images/homepage/neiro.png"
 
 const ChatCard = ({ href }: CardProps) => {
   return (
