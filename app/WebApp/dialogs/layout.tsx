318d8d024e6e09624b3a4bee551f6935741ae50e
Sergey <spanteleev670@gmail.com>
2025-06-16 02:29:21 +0300

    fix PromoModal, dialogs layout

--- app/WebApp/dialogs/layout.tsx
+++ app/WebApp/dialogs/layout.tsx
@@ -6,7 +6,7 @@ export default function DialogsLayout({
   children: React.ReactNode
 }>) {
   return (
-    <div className="xl:px-[2%] flex-1 flex gap-[2%] bg-PageBG">
+    <div className="xl:px-[2%] flex-1 h-[100svh] flex gap-[2%] bg-PageBG">
       <Navigation />
 
       <div className="flex-1 px-[4vw] xl:px-0 xl:ml-[8%] xl:pb-0 xl:pr-0 ">
--- app/WebApp/profile/components/PromoModal.tsx
+++ app/WebApp/profile/components/PromoModal.tsx
@@ -3,12 +3,28 @@ import { usePromoStore } from "@/store/PromoStore"
 import { useState } from "react"
 import Button from "@/components/Button/Button"
 import { PaymentService } from "@/lib/services/payment.service"
+import { useEffect } from "react"
 
 const PromoModal = () => {
   const { isPromoModalOpen, closePromoModal } = usePromoStore()
   const [promoCode, setPromoCode] = useState("")
 
   const paymentService = PaymentService.getInstance()
+
+  useEffect(() => {
+    if (isPromoModalOpen) {
+      // Блокируем прокрутку
+      document.body.style.overflow = "hidden"
+    } else {
+      // Восстанавливаем прокрутку
+      document.body.style.overflow = ""
+    }
+
+    // Очистка эффекта при размонтировании или изменении isPromoModalOpen
+    return () => {
+      document.body.style.overflow = ""
+    }
+  }, [isPromoModalOpen])
 
   if (!isPromoModalOpen) return null
 
@@ -23,7 +39,7 @@ const PromoModal = () => {
   }
 
   return (
-    <div className="absolute inset-0 flex items-center justify-center bg-PrimaryBlack bg-opacity-50 z-[9999]">
+    <div className="fixed inset-0 flex items-center justify-center bg-PrimaryBlack bg-opacity-50 z-[9999]">
       <div className="bg-PageBG p-6 rounded-2xl flex flex-col gap-4 w-[300px]">
         <h3 className="text-[16px] xl:text-[18px] font-semibold">
           Введите промокод
