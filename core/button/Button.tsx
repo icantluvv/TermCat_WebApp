89998eff22da11fc40181135a358320d81b1267a
Sergey <spanteleev670@gmail.com>
2025-07-05 13:00:51 +0300

    feat(Button): добавил кор компонент кнопки

--- core/button/Button.tsx
+++ core/button/Button.tsx
@@ -1,8 +1,67 @@
-import React from "react"
+"use client"
 
-function Button() {
+import clsx from "clsx"
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from "react"
+
+const defaultButtonVariants =
+  "transition-colors duration-[.3] cursor-pointer flex items-center justify-center"
+
+const variantsButton = {
+  size: {
+    default: "",
+    big: "px-[24px] h-[48px]"
+  },
+  form: {
+    default: "",
+    rounded: "rounded-full"
+  },
+  variant: {
+    default: "",
+    primary: "bg-PrimaryGreen hover:bg-black active:bg-PrimaryGreenActive",
+    empty: "border-[1.5px] border-lightGray",
+    emptyGreen: "border-[1.5px] border-PrimaryGreen",
+    secondary: "bg-PrimaryBlack",
+    disable: "bg-lightGray"
+  }
+} as const
+
+type ButtonSize = keyof typeof variantsButton.size
+type ButtonForm = keyof typeof variantsButton.form
+type ButtonVariant = keyof typeof variantsButton.variant
+
+type ButtonProps<T extends ElementType = "button"> = {
+  as?: T
+  children?: ReactNode
+  className?: string
+  size?: ButtonSize
+  form?: ButtonForm
+  variant?: ButtonVariant
+} & ComponentPropsWithoutRef<T>
+
+function Button<T extends ElementType = "button">({
+  as,
+  className,
+  size = "default",
+  form = "default",
+  variant = "default",
+  children,
+  ...props
+}: ButtonProps<T>) {
+  const Component = as || "button"
+
   return (
-    <div></div>
+    <Component
+      className={clsx(
+        defaultButtonVariants,
+        variantsButton.size[size],
+        variantsButton.form[form],
+        variantsButton.variant[variant],
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </Component>
   )
 }
 
